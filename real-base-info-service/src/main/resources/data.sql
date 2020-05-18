8fcf4a8a176c49b235bb8a165c65f0941f938129
Suraj <iamsurajmehta@gmail.com>
2020-05-18 15:53:08 +0530

    Sample Data

--- real-base-info-service/src/main/resources/data.sql
+++ real-base-info-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO REAL_BASE_INFO(id, first_name, last_name) values (1, 'Hamed', 'Mirzaei');
-INSERT INTO REAL_BASE_INFO(id, first_name, last_name) values (2, 'Morteza', 'Moosavi');
-INSERT INTO REAL_BASE_INFO(id, first_name, last_name) values (3, 'Alireza', 'Khatamidoost');
-INSERT INTO REAL_BASE_INFO(id, first_name, last_name) values (4, 'Alireza', 'Mesgari');
+INSERT INTO REAL_BASE_INFO(id, first_name, last_name) values (1, 'Suraj', 'Mehta');
+INSERT INTO REAL_BASE_INFO(id, first_name, last_name) values (2, 'Jack', 'Brendt');
+INSERT INTO REAL_BASE_INFO(id, first_name, last_name) values (3, 'Tim', 'Payne');
+INSERT INTO REAL_BASE_INFO(id, first_name, last_name) values (4, 'Robert', 'Pesci');
